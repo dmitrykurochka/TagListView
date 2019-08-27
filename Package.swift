9be78a9f343b3a620fc6f3169ef51090995538b6
Andrew L <49048605+fplsv@users.noreply.github.com>
2019-08-27 12:34:36 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:5.0
-//  Copyright 2016 Skyscanner Ltd
+//  Copyright 2018 fplsv
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with 
 //  the License. You may obtain a copy of the License at
